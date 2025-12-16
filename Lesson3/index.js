caa20ccc3fd51a4fbdc268b3ce9dc1ae69338802
Gygi4 <gygi4@gusic.xyz>
2025-12-16 21:16:30 +0200

    Added lessons steps

--- Lesson3/index.js
+++ Lesson3/index.js
@@ -5,4 +5,39 @@
 // 3. git commit -m "Init"
 // 4. git push
 
-console.log("test");
+const files = ["cat.jpg", "dog.png", "doc.docx", "Virus.exe", "document.pdf"];
+
+for (const file of files) {
+    if (file === "cat.jpg") {
+        continue;
+    }
+
+    if (file === "Virus.exe") {
+        console.log("АЛАРМ! Вирус найден!");
+        break;
+    }
+
+    console.log(`Файл проверен: ${file}`);
+}
+
+// files.push("music.mp3");
+
+// for (const file of files) {
+//     console.log(file);
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("Отжимание номер " + i);
+// }
+
+// let fuel = 100; // топливо
+
+// while (fuel > 0) {
+//     console.log("Едем " + "Топлива осталось: " + fuel);
+//     fuel -= 10; // fuel = fuel - 10;
+// }
+
+// console.log("Приехали");
+
+// for (СТАРТ; ФИНИШ; ШАГ);
+
